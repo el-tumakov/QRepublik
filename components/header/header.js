62827de30c76cb0d2ae5827567a57e751c68693d
Егор Тумаков <el.tumakov@gmail.com>
2021-04-30 11:50:36 +0300

    Добавляет Header

---
+++ components/header/header.js
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navigation}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <Image
+              src="/images/logo.svg"
+              alt="Логотип компании QRepublik"
+              width={129}
+              height={25}
+            />
+          </a>
+        </Link>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="#">
+              <a className={styles.link}>Как это работает</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="#">
+              <a className={styles.link}>Купить свой ID</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="#">
+              <a className={styles.link}>О нас</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <button className={styles.locale} type="button">
+        RU
+      </button>
+      <ul className={styles.loginList}>
+        <li>
+          <Link href="#">
+            <a className={styles.button}>Зарегистрироваться</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="#">
+            <a className={styles.button && styles.buttonSignUp}>Войти</a>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/layout/layout.js
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Header from "../header/header";
+
+const Layout = (props) => {
+  const {children} = props;
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>QRepublik</title>
+        <link
+          rel="preload"
+          href="./fonts/roboto-regular.woff2"
+          as="font"
+          crossOrigin
+        />
+        <link
+          rel="preload"
+          href="./fonts/roboto-medium.woff2"
+          as="font"
+          crossOrigin
+        />
+        <link
+          rel="preload"
+          href="./fonts/roboto-bold.woff2"
+          as="font"
+          crossOrigin
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
